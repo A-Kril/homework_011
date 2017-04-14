a1f42ee495344289476eb674991b13b05afbbe2f
A-Kril <a.krilevets@gmail.com>
2017-04-14 22:36:48 +0300

    Initial commit

---
+++ src/homework_011.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class homework_011 {
+    public static void main(String[] args) {
+
+        Scanner name = new Scanner(System.in);
+        System.out.println("Enter Your name:");
+
+        String n = name.nextLine();
+        System.out.println("Hello, " +n);
+    }
+
+}
